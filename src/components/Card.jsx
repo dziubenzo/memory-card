6735372d3f44cdcb4e506ea1dbe4db0fa4eac4ab
dziubenzo <dziubenzo@gmail.com>
2024-02-16 16:29:40 +0100

    Implement game logic
    This includes increasing points, resetting points if a card has been
    clicked before and setting the best result.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
-export default function Card() {
+/* eslint-disable react/prop-types */
+
+export default function Card({ handleClick, keyProp }) {
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => {
+        handleClick(keyProp);
+      }}
+    >
       <div className="card-image">
-        <p>Placeholder</p>
+        <p>Placeholder {keyProp}</p>
       </div>
-      <p className="card-description">Description</p>
+      <p className="card-description">Description {keyProp}</p>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,48 @@
+import { useState } from 'react';
 import Card from './Card';
 
+const CARDS = 3;
+
 export default function Main() {
+  const [points, setPoints] = useState(0);
+  const [best, setBest] = useState(0);
+  const [clicked, setClicked] = useState(Array(CARDS).fill(false));
+
+  function createCards(cards) {
+    let cardsArray = [];
+    for (let i = 0; i < cards; i++) {
+      cardsArray.push(<Card key={i} keyProp={i} handleClick={handlePoints} />);
+    }
+    return cardsArray;
+  }
+
+  function handlePoints(cardKey) {
+    if (!clicked[cardKey]) {
+      setPoints(points + 1);
+      handleClicked(cardKey);
+    } else {
+      handleBest(points);
+      setPoints(0);
+      setClicked(Array(CARDS).fill(false));
+    }
+  }
+
+  function handleClicked(cardKey) {
+    const updatedClicked = clicked.map((item, index) => {
+      return index === cardKey ? (item = true) : item;
+    });
+    setClicked(updatedClicked);
+  }
+
+  function handleBest(points) {
+    if (points > best) {
+      setBest(points);
+    }
+  }
+
   return (
     <main>
-      <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <div className="cards">{createCards(CARDS)}</div>
     </main>
   );
 }
